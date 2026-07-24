fc432c2ccdfb033d87b62f34b2ea3e7610fba019
cowraiu <danilocarlosprado@gmail.com>
2026-07-23 22:06:26 -0400

    sei la ta funcionando a row com  os copos

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,63 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 
-import acaicopo from "./assets/acaiPhoto.png"
+import acaicopo from "./assets/pngCopo.png"
 
+import banner from "./assets/acaiMaisUsavel.png"
+
+
+export const coposAcai = [
+  {
+    tamanho: "240ml",
+    preco: 13.00,
+    foto: acaicopo
+  },
+  {
+    tamanho: "300ml",
+    preco: 16.00,
+    foto: acaicopo
+  },
+  {
+    tamanho: "500ml",
+    preco: 24.00,
+    foto: acaicopo
+  },
+  {
+    tamanho: "700ml",
+    preco: 35.00,
+    foto: acaicopo
+  }
+];
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <div className="flex justify-center">
+      <div className="scroll-row" style={{display:"flex"}}>
         <img
-        src={acaicopo}
+        src={banner}
         alt="acai"
-        className="w-270 h-110 rounded-x1"
+        className="w-270 h-130 rounded-xl"
         />
       </div>
-      <h1></h1>
+
+      <div>
+        <h1>Copos</h1>
+        <div className="scroll-row">
+
+          {coposAcai.map((copo, index) => (
+            <div key={index} className="card">
+              <img src={copo.foto || pngcopo} alt={`Copo de ${copo.tamanho}`} />
+              <h3>{copo.tamanho}</h3>
+              <p>
+                {copo.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+              </p>
+            </div>
+          ))}
+
+        </div>
+      </div>
     </main>
   )
 }
